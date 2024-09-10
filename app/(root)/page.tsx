4bb44a98abd37e80dfc5d272d05c9f97ebfe3d2d
ParagNirwan <pnirwan9@gmail.com>
2024-09-10 10:37:25 +0200

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 const Home = () => {
+
+    const loggedIn = { firstName: "Parag" };
+
+
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className="home-header">
+                    <HeaderBox type="greeting" title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and Manage your Account and Transactions efficiently" />
+
+                    <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={5450.69} />
+                </header>
+            </div>
+
+
+        </section>
     )
 }
 
