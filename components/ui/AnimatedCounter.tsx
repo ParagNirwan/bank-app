ab428306c61dc008c7bd2aeafad64a4575568551
ParagNirwan <pnirwan9@gmail.com>
2024-09-10 18:20:07 +0200

    Implement Chart

--- components/ui/AnimatedCounter.tsx
+++ components/ui/AnimatedCounter.tsx
@@ -1,13 +1,13 @@
-'use client'
-import React from 'react'
-import CountUp from 'react-countup'
+"use client";
+import React from "react";
+import CountUp from "react-countup";
 
-const AnimatedCounter = ({amount}:{amount:number}) => {
+const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div className='w-full'>
-        <CountUp end={amount} decimal=',' prefix='€' decimals={2}/>
+    <div className="w-full">
+      <CountUp end={amount} decimal="," prefix="€" decimals={2} />
     </div>
-  )
-}
+  );
+};
 
-export default AnimatedCounter
+export default AnimatedCounter;
